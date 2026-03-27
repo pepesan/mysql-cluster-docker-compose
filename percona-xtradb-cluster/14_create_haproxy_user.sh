9d763509b47941648a27fa3de0cf372872fb7ef1
David Vaquero <pepesan@gmail.com>
2026-03-27 09:25:10 +0100

    mejorado el ejemplo de percona xtradb cluster

--- percona-xtradb-cluster/14_create_haproxy_user.sh
+++ percona-xtradb-cluster/14_create_haproxy_user.sh
@@ -5,4 +5,4 @@ docker exec -it pxc-node1  mysql -u root -p'test1234#' --verbose -e \
 GRANT USAGE ON *.* TO 'haproxy_user'@'%'; \
 FLUSH PRIVILEGES;"
 
- docker compose exec pxc-node1 mysql --verbose -u root -p'test1234#' -e "SELECT user, host FROM mysql.user WHERE user='haproxy_check';"
+ docker compose exec pxc-node1 mysql --verbose -u root -p'test1234#' -e "SELECT user, host FROM mysql.user WHERE user='haproxy_user';"
